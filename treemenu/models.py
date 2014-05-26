f2228155f1287648094fcc15d20ea25fd20e8d12
Semen Pupkov <semen.pupkov@gmail.com>
2014-05-26 11:00:58 +0600

    Add python 3 str repr

--- treemenu/models.py
+++ treemenu/models.py
@@ -1,38 +1,40 @@
-# -*- coding: utf-8 -*-
-#author: Semen Pupkov (semen.pupkov@gmail.com)
+#coding: utf-8
+from __future__ import unicode_literals
 
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from django.dispatch import receiver
 from django.core.cache import cache
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Menu(models.Model):
-
-    title = models.CharField(verbose_name=u'Название меню', max_length=50)
+    title = models.CharField(verbose_name='Название меню', max_length=50)
     slug = models.SlugField()
 
     class Meta:
-        verbose_name = u'Меню'
+        verbose_name = 'Меню'
         verbose_name_plural = 'Меню'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 
+@python_2_unicode_compatible
 class MenuItem(MPTTModel):
     class Meta:
-        verbose_name = u'Пункт меню'
-        verbose_name_plural = u'Пункты меню'
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
         ordering = ['tree_id', 'lft']
 
-    active = models.BooleanField(verbose_name=u'Активность', default=True)
-    menu = models.ForeignKey(Menu, verbose_name=u'Меню', related_name='menu_items')
-    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', verbose_name=u'Родительский пункт')
-    title = models.CharField(verbose_name=u'Название  меню', max_length=50)
+    active = models.BooleanField(verbose_name='Активность', default=True)
+    menu = models.ForeignKey(Menu, verbose_name='Меню', related_name='menu_items')
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', verbose_name='Родительский пункт')
+    title = models.CharField(verbose_name='Название  меню', max_length=50)
     url = models.CharField(max_length=100, help_text='Ссылка, например /about/ или http://foo.com/')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     def title_with_spacer(self):
